4193f89a934a11fbfcf056f3401b5de0748f47f1
A.Dugelet <du.aubin@hotmail.fr>
2018-12-09 16:28:20 +0100

    adding access to database properties

--- back/ttfl_dashboard_api/properties/properties.py
+++ back/ttfl_dashboard_api/properties/properties.py
@@ -3,6 +3,15 @@ from configparser import ConfigParser
 config = ConfigParser()
 config.read('properties/properties.ini')
 APISection = 'API'
+DbSection = 'Database'
+
 
 def APIProperty(field):
     return config[APISection][field]
+
+
+def DbProperty(field, isInt=False):
+    if isInt:
+        return int(config[DbSection][field])
+    else:
+        return config[DbSection][field]
